06e9668b7705bb3f1de5cad594d02b2eb80b29b1
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-03-18 20:02:15 +0300

    settings changes

--- redflag/ios/Runner/AppDelegate.swift
+++ redflag/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC117n24-9adBlndIKy6k66D3oY1kjR_fc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
