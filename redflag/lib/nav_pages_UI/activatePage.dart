ef75f8f8979689c4a8031caf050b5a1dd3b5f526
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-03-24 23:10:18 +0300

    Emergency button UI

--- redflag/lib/nav_pages_UI/activatePage.dart
+++ redflag/lib/nav_pages_UI/activatePage.dart
@@ -11,16 +11,48 @@ class activatePage extends StatefulWidget {
 class _activatePageState extends State<activatePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.red,
+    return Scaffold(
         body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  UserLocation().getLocation();
-                },
-                child: Text('Activate'))),
-      ),
-    );
+      child: Container(
+          alignment: Alignment.center,
+          margin: const EdgeInsets.only(top: 130.0, bottom: 20.0),
+          padding: EdgeInsets.all(50),
+          child: Column(children: [
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(colors: [
+                      Colors.redAccent,
+                      Color.fromARGB(255, 241, 114, 114)
+                      //add more colors
+                    ]),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Color.fromARGB(
+                              145, 63, 63, 63), //shadow for button
+                          blurRadius: 10 //blur radius of shadow
+                          )
+                    ]),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    onSurface: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    //make color or elevated button transparent
+                    fixedSize: const Size(500, 300), // button size
+                  ),
+                  onPressed: () {
+                    UserLocation().getLocation();
+                  },
+                  child: new Text(
+                    "Emergency",
+                    style: new TextStyle(
+                      fontSize: 25.0,
+                      color: Color.fromARGB(255, 241, 226, 226),
+                    ),
+                  ),
+                ))
+          ])),
+    ));
   }
 }
