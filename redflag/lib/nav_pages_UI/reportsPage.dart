710b3a9ae434ac2f7e2dcdf8cbc753edf754914b
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-04-02 05:30:42 +0300

    AppBar in report page

--- redflag/lib/nav_pages_UI/reportsPage.dart
+++ redflag/lib/nav_pages_UI/reportsPage.dart
@@ -12,9 +12,58 @@ class _reportsPageState extends State<reportsPage> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 139, 255, 143),
-        body: Center(child: Text("reportsPage")),
-      ),
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          body: Stack(
+            children: [
+//------------------------------------ App Bar ----------------------------------------
+
+              Container(
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(30)),
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 122, 122, 243),
+                      Color(0xFF4E4EF7)
+                    ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+              ),
+
+//------------------------------------ User info ----------------------------------------
+
+              Container(
+                padding: EdgeInsets.only(top: 80, left: 120),
+                child: Column(
+                  children: [
+                    //child 1 --> avatar
+                    CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    //child 2 --> for space between the avatar and user data
+                    SizedBox(
+                      height: 20,
+                    ),
+//----------------//child 3 --> user data
+                    Column(
+                      children: [
+                        Text(
+                          'Atheer Alghamdi',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17),
+                        ),
+                        Text(
+                          '055-834-2221',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
