097210c0b2e28857b1493e7d351a2400d317f895
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-04-02 05:22:43 +0300

    Delete dynamic add and replace with non dynamic

--- redflag/lib/nav_pages_UI/add_ec.dart
+++ redflag/lib/nav_pages_UI/add_ec.dart
@@ -1,145 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class add extends StatefulWidget {
+  const add({Key? key}) : super(key: key);
+
   @override
-  _addState createState() => _addState();
+  State<add> createState() => _addState();
 }
 
 class _addState extends State<add> {
-  List<DynamicWidget> listDynamic = [];
-  List<String> fullNme = [];
-  List<String> phone = [];
+  TextEditingController controller_fullName =
+      new TextEditingController(); // controll the TextField
+  TextEditingController controller_phone = new TextEditingController();
 
-  Icon floatingIcon = new Icon(Icons.add);
-
-  addDynamic() {
-    if (fullNme.length != 0) {
-      floatingIcon = new Icon(Icons.add);
-
-      fullNme = [];
-      listDynamic = [];
-      print('if');
-    }
-    setState(() {});
-    if (listDynamic.length >= 10) {
-      return;
-    }
-    listDynamic.add(new DynamicWidget());
-  }
+  String? name;
+  String? phone;
 
   submitData() {
-    floatingIcon = new Icon(Icons.arrow_back);
-    fullNme = [];
-    phone = [];
-    listDynamic
-        .forEach((widget) => fullNme.add(widget.controller_fullName.text));
-    listDynamic.forEach((widget) => phone.add(widget.controller_phone.text));
+    name = controller_fullName.text;
+    phone = controller_fullName.text;
 
     setState(() {});
-    print(fullNme.length);
-    print(phone.length);
+    print(name);
+    print(phone);
   }
 
   @override
-  Widget build(BuildContext context) {
-    Widget result = new Flexible(
-        flex: 1,
-        child: new Card(
-          child: ListView.builder(
-            itemCount: fullNme.length,
-            itemBuilder: (_, index) {
-              return new Padding(
-                padding: new EdgeInsets.all(10.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Container(
-                          margin: new EdgeInsets.only(left: 10.0),
-                          child: new Text(
-                              "Full Name ${index + 1} : ${fullNme[index]}"),
-                        ),
-                        Container(
-                          margin: new EdgeInsets.only(left: 10.0),
-                          child: new Text(
-                              "Phone Number ${index + 1} : ${phone[index]}"),
-                        ),
-                      ],
-                    ),
-                    new Divider()
-                  ],
-                ),
-              );
-            },
-          ),
-        ));
-
-    Widget dynamicTextField = new Flexible(
-      flex: 2,
-      child: new ListView.builder(
-        itemCount: listDynamic.length,
-        itemBuilder: (_, index) => listDynamic[index],
-      ),
-    );
-
-    Widget submitButton = new Container(
-      child: new ElevatedButton(
-          onPressed: submitData,
-          child: new Padding(
-            padding: new EdgeInsets.all(16.0),
-            child: new Text('Save Emergency Contact'),
-          ),
-          style: ElevatedButton.styleFrom(
-            primary: Color(0xFF4E4EF7),
-          )),
-    );
-
-    return new MaterialApp(
-      home: new Scaffold(
-        // appBar: new AppBar(
-        //   title: new Text('Add Emergency contacts'),
-        // ),
-
-        body: new Container(
-          margin: new EdgeInsets.all(10.0),
-          child: new Column(
-            children: <Widget>[
-              fullNme.length == 0 ? dynamicTextField : result,
-              fullNme.length == 0 ? submitButton : new Container(),
-            ],
-          ),
-        ),
-        floatingActionButton: new FloatingActionButton(
-          onPressed: addDynamic,
-          child: floatingIcon,
-          backgroundColor: Colors.red[200],
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
   }
-}
-
-class DynamicWidget extends StatelessWidget {
-  TextEditingController controller_fullName = new TextEditingController();
-  TextEditingController controller_phone = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: new EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller_fullName,
-            decoration: new InputDecoration(hintText: 'Full Name '),
-          ),
-          TextField(
-            controller: controller_phone,
-            decoration: new InputDecoration(hintText: 'Phone Number '),
-          ),
-        ],
-      ),
+      child: Scaffold(
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          body: Stack(
+            children: [
+//------------------------------------ App Bar ----------------------------------------
+
+              Container(
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(30)),
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 122, 122, 243),
+                      Color(0xFF4E4EF7)
+                    ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+              ),
+
+//------------------------------------ User info ----------------------------------------
+
+              Container(
+                padding: EdgeInsets.only(top: 80, left: 120),
+                child: Column(
+                  children: [
+                    //child 1 --> avatar
+                    CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    //child 2 --> for space between the avatar and user data
+                    SizedBox(
+                      height: 20,
+                    ),
+//----------------//child 3 --> user data
+                    Column(
+                      children: [
+                        Text(
+                          'Atheer Alghamdi',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17),
+                        ),
+                        Text(
+                          '055-834-2221',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+//------------------------------------ Current Emergency Contacts List ----------------------------------------
+
+              Column(
+                children: [
+                  SizedBox(
+                    height: 300,
+                  ),
+                  Align(
+                    // to force it to stay in the left then will control it with th margin, if i remove it, it will go the center and the margin can't move it
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 35),
+                      child: Text('Add Emergency \nContacts ',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Color(0xFF4E4EF7),
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 35, right: 35),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: controller_fullName,
+                          decoration: InputDecoration(hintText: 'Full Name '),
+                        ),
+                        TextField(
+                          controller: controller_phone,
+                          decoration:
+                              InputDecoration(hintText: 'Phone Number '),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 150,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceEvenly, // space between the buttons
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 25, right: 25, top: 15, bottom: 15),
+                            child: Text('Add',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255))),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFF4E4EF7),
+                          ),
+                        ),
+                        ElevatedButton(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 25, right: 25, top: 15, bottom: 15),
+                            child: Text(
+                              'Clear',
+                              style: TextStyle(
+                                  color: Color(0xFF84A2AF),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          onPressed: null,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
