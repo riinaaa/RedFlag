8dbdb96ca49030e69bb0f7982cd7fe169a36b091
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-04-10 03:10:14 +0300

    confirmation email to EC (add page) --> DONE

--- redflag/lib/nav_pages_UI/add_ec.dart
+++ redflag/lib/nav_pages_UI/add_ec.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:redflag/Emergency.dart';
 import 'package:redflag/Users.dart';
 import 'package:redflag/registration_pages/login_screen.dart';
 import 'package:redflag/EmergencyContacts.dart';
@@ -60,6 +61,23 @@ class _addState extends State<add> {
           .set(emergencyContactModel.toMap(user!.uid));
 
       Fluttertoast.showToast(msg: "Emergency contact added successfully :) ");
+
+      //----------------------------------------
+      // Send an email to the emergency contact
+      Emergency mail = new Emergency();
+      String subject =
+          'You have been added as an Emergency Contact :: Redflag Team';
+      String ecName = emergencyContactNameEditingController.text;
+      String userFirstName = loggedInUser.getUserFirstName;
+      String userLastName = loggedInUser.getUserLastName;
+      // String recipients = emergencyContactEmailEditingController.text;
+      final recipients = <dynamic>[email];
+
+      String msg =
+          '<h1>Hello, $ecName </h1>\n<p><strong>$userFirstName $userLastName </strong>has added you as an emergency contact.\n</p><p>If there is an emergency, the Redflag team will send you the location of <strong>$userFirstName</strong>.</p>\n<br>\<br>\n<br>\n<br>\n<br>\n<hr>\n<p style="color:#6c63ff; font-family:Arial, Helvetica, sans-serif; font-size:18px;";><strong>Atheer Alghamdi</strong></p>\<p style="font-family:Arial, Helvetica, sans-serif; font-size:15px;"><strong>Redflag Developer | IT Department </strong></p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Email: redflagapp.8@gmail.com</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Adress: King Abdulaziz University | FCIT</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Websit: <a href="https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php">https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php</a></p>\n<br>\n<br>';
+      mail.sendMail(recipients, subject, msg);
+
+      //----------------------------------------
       clearData();
     }
 
