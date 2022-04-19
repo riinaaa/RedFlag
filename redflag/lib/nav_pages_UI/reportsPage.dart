28c21954cc37c4932d1b97da065da49a0279a4d8
atheer1419 <75436867+atheer1419@users.noreply.github.com>
2022-04-19 06:39:48 +0300

    .

--- redflag/lib/nav_pages_UI/reportsPage.dart
+++ redflag/lib/nav_pages_UI/reportsPage.dart
@@ -6,10 +6,12 @@ import 'package:mailer/smtp_server/gmail.dart';
 import 'package:redflag/Emergency.dart';
 import 'package:redflag/EmergencyContacts.dart';
 import 'package:redflag/Users.dart';
+import 'package:redflag/nav_pages_UI/reports/emergencyCasesList.dart';
 import 'package:redflag/registration_pages/login_screen.dart';
 
 class reportsPage extends StatefulWidget {
-  const reportsPage({Key? key}) : super(key: key);
+  final String caseNumb;
+  const reportsPage({Key? key, required this.caseNumb}) : super(key: key);
 
   @override
   State<reportsPage> createState() => _reportsPageState();
@@ -17,16 +19,9 @@ class reportsPage extends StatefulWidget {
 
 class _reportsPageState extends State<reportsPage> {
   User? user = FirebaseAuth.instance.currentUser;
-  Users loggedInUser = Users();
-  EmergencyContacts emergencyContactModel = EmergencyContacts();
 
-  Emergency mail = new Emergency();
-  String subject =
-      'You have been added as an Emergency Contact :: Redflag Team';
-  String ecName = 'Atheer';
-  String userFirstName = 'Shimma';
-  String userLastName = 'Alghamdi';
-  List<dynamic> recipients = <dynamic>[];
+  Users loggedInUser = Users();
+  Emergency caseInfo = Emergency();
 
   // String msg =
   //     '<h1>Hello, $ecName </h1>\n<p><strong>$userFirstName $userLastName </strong>has added you as an emergency contact.\n</p><p>If there is an emergency, the Redflag team will send you the location of <strong>$userFirstName</strong>.</p>\n<br>\<br>\n<br>\n<br>\n<br>\n<hr>\n<p style="color:#6c63ff; font-family:Arial, Helvetica, sans-serif; font-size:18px;";><strong>Atheer Alghamdi</strong></p>\<p style="font-family:Arial, Helvetica, sans-serif; font-size:15px;"><strong>Redflag Developer | IT Department </strong></p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Email: redflagapp.8@gmail.com</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Adress: King Abdulaziz University | FCIT</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Websit: <a href="https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php">https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php</a></p>\n<br>\n<br>';
@@ -35,62 +30,31 @@ class _reportsPageState extends State<reportsPage> {
   @override
   void initState() {
     super.initState();
+    // if(widget.caseNumb == caseInfo.caseNumber){
+
+    // }
     FirebaseFirestore.instance
-        .collection('emergencyContacts')
-        .where('user', isEqualTo: user!.uid)
+        .collection("emergencyCase")
+        .doc(user!.uid)
         .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((doc) {
-        //emergency contact
-        emergencyContactModel.eFullName = doc['eFullName'];
-        emergencyContactModel.ecEmail = doc['ecEmail'];
-        // print(emergencyContactModel.ecEmail);
-
-        // for (var i = 0; i < loggedInUser.emergencyContacts.length; i++) {
-        //   recipients = [emergencyContactModel.ecEmail];
-        // }
-        // print('-------------------------');
-
-        // print('rec --> $recipients');
-        loggedInUser.emergencyContacts.add(emergencyContactModel);
-        // List<dynamic> temp = <dynamic>[];
-        int count = 0;
-        for (var i = 0; i < 1; i++) {
-          print('i--> $i');
-          print(loggedInUser.emergencyContacts[i].eFullName);
-          print(loggedInUser.emergencyContacts[i].ecEmail);
-          print('-------------------------');
-
-          recipients.add(loggedInUser.emergencyContacts[i].ecEmail);
-        }
-        // for (var i = 0; i < loggedInUser.emergencyContacts.length; i++) {
-        //   // print(ecNum);
-        //   print(loggedInUser.emergencyContacts[i].eFullName);
-        //   print(loggedInUser.emergencyContacts[i].ecEmail);
-        //   print('-------------------------');
-
-        //   recipients = [loggedInUser.emergencyContacts[i].ecEmail];
-        // }
-
-        // print(recipients.length);
-      });
-      print(loggedInUser.emergencyContacts.length);
-      // recipients = [loggedInUser.getEmergencyContacts];
-
-      print(recipients);
-      print(recipients.length);
+        .then((value) {
+      this.caseInfo = Emergency.fromMap(value.data());
+      setState(() {});
     });
-
-    // FirebaseFirestore.instance
-    //     .collection("emergencyContacts")
-    //     .doc(user!.uid)
-    //     .get()
-    //     .then((value) {
-    //   this.loggedInEmergencyContacts = EmergencyContacts.fromMap(value.data());
-    //   setState(() {});
-    // });
-
-    super.initState();
+    //-------------------------
+    // StreamBuilder<QuerySnapshot>(
+    //     stream: FirebaseFirestore.instance
+    //         .collection('emergencyCase')
+    //         .where('user', isEqualTo: user!.uid)
+    //         .snapshots(),
+    //     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+    //       snapshot.data!.docs.map((DocumentSnapshot document) {
+    //         Map<String, dynamic> data =
+    //             document.data()! as Map<String, dynamic>;
+    //       });
+    //       return Text("Good");
+    //     });
+    //--------------------------
     FirebaseFirestore.instance
         .collection("users")
         .doc(user!.uid)
@@ -156,10 +120,25 @@ class _reportsPageState extends State<reportsPage> {
                 ),
               ),
 
+              //------------------------------------Return to the case numbers list------------------------------------
+
+              Container(
+                margin: EdgeInsets.only(top: 80, left: 15),
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_rounded),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const cemrgencyCases()),
+                    );
+                  },
+                ),
+              ),
               //------------------------------------Logout------------------------------------
 
               Container(
-                padding: EdgeInsets.only(top: 80, left: 15),
+                padding: EdgeInsets.only(top: 80, left: 300),
                 child: TextButton(
                   onPressed: () {
                     logout(context);
@@ -171,40 +150,96 @@ class _reportsPageState extends State<reportsPage> {
                       )),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(top: 400, left: 15),
-                child: ElevatedButton(
-                    onPressed: () {
-                      // for (var i = 0;
-                      //     i < loggedInUser.emergencyContacts.length;
-                      //     i++) {
-                      //   // print(ecNum);
-                      //   print(loggedInUser.emergencyContacts[i].getEcEmail);
-                      //   // print(loggedInUser.emergencyContacts[i].eFullName);
-                      //   // print(loggedInUser.emergencyContacts[i].ecEmail);
-                      //   print('-------------------------');
-                      //   recipients = [
-                      //     loggedInUser.emergencyContacts[i].ecEmail
-                      //   ];
-                      // }
-                      // print(recipients);
 
-                      mail.sendMail(recipients, subject,
-                          '<h1>Hello, $ecName </h1>\n<p><strong>$userFirstName $userLastName </strong>has added you as an emergency contact.\n</p><p>If there is an emergency, the Redflag team will send you the location of <strong>$userFirstName</strong>.</p>\n<br>\<br>\n<br>\n<br>\n<br>\n<hr>\n<p style="color:#6c63ff; font-family:Arial, Helvetica, sans-serif; font-size:18px;";><strong>Atheer Alghamdi</strong></p>\<p style="font-family:Arial, Helvetica, sans-serif; font-size:15px;"><strong>Redflag Developer | IT Department </strong></p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Email: redflagapp.8@gmail.com</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Adress: King Abdulaziz University | FCIT</p>\n<p style="font-family:Arial, Helvetica, sans-serif; font-size:12px;">Websit: <a href="https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php">https://fcitweb.kau.edu.sa/fcitwebsite/itdepartment.php</a></p>\n<br>\n<br>');
-                      // print(loggedInUser.getEmergencyContacts);
-                      // print('After clicking the button');
-                      // print(recipients);
+              //------------------------------------Test------------------------------------
+              // Container(
+              //   margin: EdgeInsets.only(top: 300, left: 15),
+              //   child: ElevatedButton(
+              //       onPressed: () {
+              //         print(data['caseNumber']);
+              //       },
+              //       child: Text('print case numer')),
+              // ),
 
-                      // for (var i = 0;
-                      //     i < loggedInUser.emergencyContacts.length;
-                      //     i++) {
-                      //   // print(ecNum);
-                      //   print(loggedInUser.emergencyContacts[i].eFullName);
-                      //   print(loggedInUser.emergencyContacts[i].ecEmail);
-                      // }
-                    },
-                    child: Text('Send Email')),
-              )
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('emergencyCase')
+                    .where('user', isEqualTo: user!.uid)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("Loading");
+                  }
+                  return ListView(
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+                      return ListTile(
+                        title: Text('Case Number ' + data['caseNumber']),
+                        leading: Icon(Icons.insert_drive_file_rounded),
+                        trailing: IconButton(
+                          icon: Icon(Icons.arrow_forward_rounded),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => reportsPage(
+                                        caseNumb: data['caseNumber'],
+                                      )),
+                            );
+                          },
+                        ),
+                        // subtitle: Text(data['ecEmail']),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+              //--------------------------------------------------
+
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 30, right: 30, top: 22, bottom: 22),
+                    margin: EdgeInsets.only(top: 220, left: 90),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      // boxShadow: shadowList,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(170, 188, 188, 188)
+                              .withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 0), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Text('${caseInfo.getCaseNumber}',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF6666FF))),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text('Emergency \nContacts.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500)),
+                ],
+              ),
             ],
           )),
     );
